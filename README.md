ea343c14da92b2ae2fac489875d95a554ec9ea97
michlee1337 <michlee1337@gmail.com>
2020-09-27 22:10:56 -0700

    get db url from env

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ View the latest production code [here](https://panorama-pro.herokuapp.com/).
 # Run
 0. Install [Python3](https://www.python.org/downloads/)
 1. Start pip env: `source env/bin/activate`
-2. Run app: `python app.py`
+2. Run app: `python src/app.py`
--- src/app.py
+++ src/app.py
@@ -1,27 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
+import os
 
 # _____ INIT + CONFIG ______
 #Create a flask app
 sys.path.append('src')
 app = Flask(__name__)
+
 #app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
-# setup and create database
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 # create database tables
-# db.init_app(app)
-# with app.app_context():
-#     db.create_all()
-#     db.session.commit()
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 # _____ TEMP ______
 
 @app.route('/')
 def hello():
-    return "Hello World! src"
+    return "Hello World!"
 
 
 # _____ MAIN ______
