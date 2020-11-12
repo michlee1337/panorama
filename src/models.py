fe0185789b79962df9ccfbfdb8c32b30816edfb4
michlee1337 <michlee1337@gmail.com>
2020-11-11 19:59:38 -0800

    study plan models

--- src/models.py
+++ src/models.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from flask_login import LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.ext.orderinglist import ordering_list
+from sqlalchemy.ext.associationproxy import association_proxy
 
 from src import app, db, login
 
@@ -58,3 +60,27 @@ class Concept(db.Model):
         secondaryjoin=id == relationships.c.ParentId,
         backref=backref('children')
     )
+
+class StudyPlanResource(db.Model):  # workaround to use ordering_list
+    __tablename__ = 'studyplan_resources'
+    id = db.Column(db.Integer, primary_key=True)
+    studyplan_chunk_id = db.Column(db.Integer, ForeignKey('studyplan_chunks.id'))
+    position = db.Column(db.Integer)
+    resource_id = db.Column(db.Integer, ForeignKey('resources.id'))
+    resource = db.relationship('Resource')
+
+class StudyPlanChunk(db.Model):
+    __tablename__ = 'studyplan_chunks'
+    id = Column(db.Integer, primary_key=True)
+    studyplan_id = db.Column(db.Integer, ForeignKey('studyplans.id'))
+    position = db.Column(db.Integer)
+    _resources = db.relationship("StudyPlanResource", order_by=[StudyPlanResource.position], collection_class=ordering_list('position'))
+    resources = association_proxy('_resources', 'resources')
+
+class StudyPlan(db.Model):
+    __tablename__ = 'studyplans'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(1000))
+    chunks = db.relationship("StudyPlanChunk", order_by=[StudyPlanChunk.position],
+                        collection_class=ordering_list('position'))
