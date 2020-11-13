156dfb0b8c81597cd613ef3d2d99e88e9db71766
michlee1337 <michlee1337@gmail.com>
2020-11-12 17:59:34 -0800

    dwag drop resources

--- src/static/js/concepts/view.js
+++ src/static/js/concepts/view.js
@@ -5,3 +5,34 @@ $(document).ready(function () {
     });
 
 });
+
+// append child for a week
+function addWeek() {
+  console.log('yo');
+}
+
+// // append child for a resource
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("resource_id", ev.target.id);
+  ev.dataTransfer.setData("resource_name", ev.target.text);
+  console.log("dragging")
+  console.log(ev.dataTransfer.getData("resource_id"));
+  console.log(ev.dataTransfer.getData("resource_name"));
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var resource_id = ev.dataTransfer.getData("resource_id");
+  var resource_name = ev.dataTransfer.getData("resource_name");
+  var resource = document.createElement('div'); // is a node
+  if (resource_name != "undefined") {
+    $(resource).attr("id", resource_id);
+    $(resource).text(resource_name);
+    $(resource).attr("class", "studyplan_creator_week_resources_card btn-success");
+    ev.target.appendChild(resource);
+  }
+}
