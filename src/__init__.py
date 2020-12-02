4175314cfaea5895e67a2e7dcc36a62801a4cd11
michlee1337 <michlee1337@gmail.com>
2020-12-01 17:02:46 -0800

    rmv old dev code

--- src/__init__.py
+++ src/__init__.py
@@ -36,33 +36,6 @@ from src.models import User
 def load_user(id):
     return User.query.get(int(id))
 
-# TEST
-# concept1 = models.Concept(
-#     title = "Concept 1")
-#
-# concept2 = models.Concept(
-#     title = "Concept 2")
-#
-# concept1.children.append(concept2)
-#
-# resource1 = models.Resource(name="google", link = "www.google.com", depth=0)
-# resource2 = models.Resource(name="purple", link = "www.purple.com", depth=1)
-# resource3 = models.Resource(name="github", link = "www.github.com", depth=2)
-# concept1.resources.append(resource1)
-# concept1.resources.append(resource2)
-# concept2.resources.append(resource3)
-# db.session.merge(resource1)
-# db.session.merge(resource2)
-# db.session.merge(resource3)
-# db.session.merge(concept1)
-# db.session.merge(concept2)
-#
-# db.session.commit()
-
-# END TEST
-
-
-
 # register routes
 # from blueprints.concepts import concepts_template
 from blueprints.users import users_template
@@ -73,29 +46,6 @@ from blueprints.pages import pages_template
 app.register_blueprint(users_template)
 app.register_blueprint(studyplans_template)
 app.register_blueprint(pages_template)
-
-# _____ TEMP ______
-# @app.route('/')
-# def hello():
-#     return "Hello World! View <a href='concepts/1'>demo page </a>"
-#
-# @app.route('/concept_test')
-# def concept_test():
-#
-#     cs = Concept.query.all()
-#     print([str(c) for c in cs])
-#     ctitle = [c.title for c in cs]
-#     return str(ctitle)
-#
-# @app.route('/resource_test')
-# def resource_test():
-#
-#     cs = Resource.query.all()
-#     print([str(c) for c in cs])
-#     ctitle = [c.name for c in cs]
-#     return str(ctitle)
-#
-# _____ END TEMP ______
 
 # _____ MAIN ______
 
