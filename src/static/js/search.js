74e0b427d5ccd10f4bb02bf5e23cd36fdb27dc88
michlee1337 <michlee1337@gmail.com>
2020-12-11 17:13:37 -0800

    toggle search

---
+++ src/static/js/search.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+  drowndown_btn = document.getElementById("dropdownMenuButton");
+  drowndown_btn.addEventListener ("click", toggleFilter);
+});
+
+function toggleFilter() {
+  search_filter = document.getElementById('search-filter');
+  if (search_filter.style.display == "block") {
+    search_filter.style.display = "none";
+    $("#search-term-input").attr("placeholder","search studyplans")
+  } else {
+    search_filter.style.display = "block";
+    $("#search-term-input").attr("placeholder","search resources")
+  }
+}
