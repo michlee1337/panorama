85703308aea41b6dd8dcaacdff41f6405dc3f176
michlee1337 <michlee1337@gmail.com>
2021-01-13 14:17:49 -0800

    rmv all hidden inputs

--- src/static/js/artifacts/new.js
+++ src/static/js/artifacts/new.js
@@ -1,21 +1,6 @@
-// ______ STUDYPLAN CREATOR INTERNAL STATE ______
-let chunk_names = [],
-  chunk_descriptions = [],
-  chunk_contents = [],
-  chunk_concepts = [],
-  prereqs = ['enter as tags'],
-  chunk_idx = 0;
 
 $( document ).ready(function() {
-  // ______ HIDDEN INPUTS ______
-  let hiddenInput_prereqs = document.getElementById('hiddenInput_prereqs'),
-  hiddenInput_chunk_names = document.getElementById('hiddenInput_chunk_names'),
-  hiddenInput_chunk_descriptions = document.getElementById('hiddenInput_chunk_descriptions'),
-  hiddenInput_chunk_contents = document.getElementById('hiddenInput_chunk_contents');
-  hiddenInput_chunk_concepts = document.getElementById('hiddenInput_chunk_concepts');
-
   new Taggle('prereqs-taggle', {
-    tags: prereqs,
     duplicateTagClass: 'bounce',
     hiddenInputName: 'prereqs[]',
   });
@@ -27,55 +12,21 @@ $( document ).ready(function() {
 var chunk_card_template = `<div class="artifacts-creator_chunk card">
     <div class="form-group">
       <h4>
-        <input type="text" onChange="UpdateChunk(%CHUNK_IDX%, this.value)" placeholder="Title">
+        <input type="text" name="chunk_titles[]" placeholder="Title">
       </h4>
       <h3>
-        <input type="text" onChange="UpdateChunkConcept(%CHUNK_IDX%, this.value)" placeholder="concept"></h4>
+        <input type="text" name="chunk_concepts[]" placeholder="concept"></h4>
       </h3>
     </div>
     <div class="form-group">
-      <input type="textarea" onChange="UpdateChunkDescription(%CHUNK_IDX%, this.value)" placeholder="description">
+      <input type="textarea" name="chunk_descriptions[]" placeholder="description">
     </div>
     <div class="form-group">
-      <input type="textarea" rows="15" onChange="UpdateChunkContent(%CHUNK_IDX%, this.value)" placeholder="Type content here">
+      <input type="textarea" rows="15" name="chunk_contents[]" placeholder="Type content here">
     </div>
 
   </div>`;
 
 function addChunk() {
-  var chunk_card = chunk_card_template;
-  chunk_card = chunk_card_template.replaceAll("%CHUNK_IDX%", chunk_idx);
-  $("#artifacts-creator_wrapper").append(chunk_card);
-  chunk_idx += 1;
-  chunk_names.push(null);
-  chunk_descriptions.push(null);
-  chunk_contents.push(null);
-  console.log(chunk_names, chunk_descriptions, chunk_contents);
-}
-
-// ______ UPDATE HIDDEN FIELDS _____
-function UpdateChunk(chunk_idx, chunk_name) {
-  chunk_names[chunk_idx] = chunk_name;
-}
-
-function UpdateChunkConcept(chunk_idx, chunk_concept) {
-  chunk_concepts[chunk_idx] = chunk_concept;
-}
-
-
-function UpdateChunkDescription(chunk_idx, chunk_description) {
-  chunk_descriptions[chunk_idx] = chunk_description;
-}
-
-function UpdateChunkContent(chunk_idx, chunk_content) {
-  chunk_contents[chunk_idx] = chunk_content;
-}
-
-function SubmitHiddenInputs() {
-    hiddenInput_prereqs.value = prereqs.join(',');
-    hiddenInput_chunk_names.value = chunk_names.join(',');
-    hiddenInput_chunk_descriptions.value = chunk_descriptions.join(',');
-    hiddenInput_chunk_contents.value = chunk_contents.join(',');
-    hiddenInput_chunk_concepts.value = chunk_concepts.join(',');
-    return True;
+  $("#artifacts-creator_wrapper").append(chunk_card_template);
 }
