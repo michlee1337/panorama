24e84c2de6b89376dc451d853722968bd434a504
michlee1337 <michlee1337@gmail.com>
2021-01-13 13:33:18 -0800

    hidden inputs in new.js

--- src/static/js/artifacts/new.js
+++ src/static/js/artifacts/new.js
@@ -2,18 +2,18 @@
 let chunk_names = [],
   chunk_descriptions = [],
   chunk_contents = [],
-  prereqs = [],
+  prereqs = ['enter as tags'],
   chunk_idx = 0;
 
 $( document ).ready(function() {
   // ______ HIDDEN INPUTS ______
-  // let hiddenInput_prereqs = document.getElementById('hiddenInput_prereqs'),
+  let hiddenInput_prereqs = document.getElementById('hiddenInput_prereqs'),
   hiddenInput_chunk_names = document.getElementById('hiddenInput_chunk_names'),
   hiddenInput_chunk_descriptions = document.getElementById('hiddenInput_chunk_descriptions'),
   hiddenInput_chunk_contents = document.getElementById('hiddenInput_chunk_contents');
 
   new Taggle('prereqs-taggle', {
-    tags: ['Try', 'entering', 'one', 'of', 'these', 'tags'],
+    tags: prereqs,
     duplicateTagClass: 'bounce'
   });
 });
@@ -44,21 +44,26 @@ function addChunk() {
   $("#artifacts-creator_wrapper").append(chunk_card);
   chunk_idx += 1;
   chunk_names.push(null);
-  hiddenInput_chunk_names.value = chunk_names.join(',');
 }
 
 // ______ UPDATE HIDDEN FIELDS _____
 function UpdateChunk(chunk_idx, chunk_name) {
   chunk_names[chunk_idx] = chunk_name;
-  hiddenInput_chunk_names.value = chunk_names.join(',');
 }
 
 function UpdateChunkDescription(chunk_idx, chunk_description) {
   chunk_descriptions[chunk_idx] = chunk_description;
-  hiddenInput_chunk_descriptions.value = chunk_descriptions.join(',');
 }
 
 function UpdateChunkContent(chunk_idx, chunk_content) {
   reading_names[reading_idx] = reading_name;
+}
+
+function SubmitHiddenInputs() {
+  hiddenInput_prereqs.value = prereqs.join(',');
+  hiddenInput_chunk_names.value = chunk_names.join(',');
+  hiddenInput_chunk_names.value = chunk_names.join(',');
+  hiddenInput_chunk_descriptions.value = chunk_descriptions.join(',');
   hiddenInput_reading_names.value = reading_names.join(',');
+  return True;
 }
