b7af578fb2ced54cc70884e84b979391c03aec0c
michlee1337 <michlee1337@gmail.com>
2021-01-20 17:04:19 -0800

    render artifacts/new

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -9,5 +9,9 @@ class TestPageCase(FlaskTestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_create_page(self):
+        response = self.app.get('/artifacts/new', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
 if __name__ == '__main__':
     unittest.main()
